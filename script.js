37126d76f55f39e9b5d5c0e488cc4479c1ff3e04
victorhreinert <victorhreinert@gmail.com>
2024-01-29 10:49:38 -0300

    Finalização do desenvolvimento JavaScript e estilização com CSS

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-const resultArtist = document.getElementById("result-artist");
-const playlistContainer = document.getElementById("result-playlists");
-const searchInput = document.getElementById("search-input");
+const h1 = document.querySelector('#greeting');
 
-function requestApi(searchTerm) {
-    fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
-        .then((response) => response.json())
-        .then((results) => displayResults(results));
+var horaAtual = new Date().getHours();
+
+/* document.body.h1.document.querySelector('#h1') */
+
+if (horaAtual > 5 && horaAtual < 13) {
+    h1.innerText = `Bom Dia`;
+    console.log('Bom Dia`');
+} else if (horaAtual > 13 && horaAtual < 20) {
+    h1.innerText = `Boa Tarde`;
+    console.log('Boa Tarde');
+} else {
+    h1.innerText = `Boa Noite`;
+    console.log('Boa Noite');
 }
 
-function displayResults(results) {
-    hidePlaylists();
-    const artistImage = document.getElementById("artist-img");
-    const artistName = document.getElementById("artist-name");
+// GRID INTELIGENTE
+const container = document.querySelector(".offer__list-item");
 
-    results.forEach((element) => {
-        artistImage.src = element.urlImg;
-        artistName.innerText = element.name;
-    });
-    resultArtist.classList.remove("hidden");
-}
+const observer = new ResizeObserver(() => {  //mudanças no tamanho do elemento 
+  const containerWidth = container.offsetWidth; //largura total do elemento, incluindo largura do conteúdo, bordas e preenchimento.
+  const numColumns = Math.floor(containerWidth / 200); //número de colunas com base na largura do container
 
-function hidePlaylists() {
-    playlistContainer.classList.add("hidden");
-}
+  //largura mínima de 200px e máxima de 1fr (uma fração do espaço disponível).
+  container.style.gridTemplateColumns = `repeat(${numColumns}, minmax(200px, 1fr))`;
 
-searchInput.addEventListener("input", function () {
-    const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === "") {
-        resultArtist.classList.add("hidden");
-        playlistContainer.classList.remove("hidden");
-        return;
-    }
-    requestApi(searchTerm);
+  console.log({ container });
+  console.log({ numColumns });
 });
+//observando a mudança do elemento
+observer.observe(container);
